fc2349262875633ac8404e67aa8aac9bd71f0b7e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-07 11:43:36 +0300

    fix the flexboxes and set the pages navigation

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1,3 +1,32 @@
+//pages navigation
+$('#nav_homework').click(() => {
+    $('#events').css('display', 'none');
+    $('#distribution').css('display', 'none');
+    $('#homework').css('display', 'flex');
+    $('#nav_homework').addClass('active_nav');
+    $('#nav_events').removeClass('active_nav');
+    $('#nav_distribution').removeClass('active_nav');
+});
+
+$('#nav_events').click(() => {
+    $('#events').css('display', 'flex');
+    $('#distribution').css('display', 'none');
+    $('#homework').css('display', 'none');
+    $('#nav_homework').removeClass('active_nav');
+    $('#nav_events').addClass('active_nav');
+    $('#nav_distribution').removeClass('active_nav');
+});
+
+$('#nav_distribution').click(() => {
+    $('#events').css('display', 'none');
+    $('#distribution').css('display', 'flex');
+    $('#homework').css('display', 'none');
+    $('#nav_homework').removeClass('active_nav');
+    $('#nav_events').removeClass('active_nav');
+    $('#nav_distribution').addClass('active_nav');
+});
+
+
 // $(document).ready(function() {
 //     $('.setChanges_btn').click(async function() {
 //         const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
