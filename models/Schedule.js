d7648e7889222680e911032a738b0b1d86e113d6
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-17 14:19:08 +0300

    modify the schedule schema, but comment the start and end time to use in future updates

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
-const schedueSchema = new mongoose.Schema({
+const scheduleSchema = new mongoose.Schema({
     subject: { type: String, required: true },
+    // startTime: { type: String, required: true },
+    // endTime: { type: String, required: true } 
 });
 
 const dayScheduleSchema = new mongoose.Schema({
-    date: { type: Date, required: true },
-    subjects: [schedueSchema]
+    date: { type: Date, required: true, unique: true, index: true },
+    subjects: [scheduleSchema]
 });
 
-const schedule = new mongoose.Schema({
-    monday: dayScheduleSchema,
-    tuesday: dayScheduleSchema,
-    wednesday: dayScheduleSchema,
-    thursday: dayScheduleSchema,
-    friday: dayScheduleSchema
+const weekScheduleSchema = new mongoose.Schema({
+    days: [dayScheduleSchema]
 });
